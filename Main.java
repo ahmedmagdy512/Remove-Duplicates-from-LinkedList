2e4d7111f72e4f3e1f5748ac65ed6bcdd38082ec
ahmedmagdy512 <45365052+ahmedmagdy512@users.noreply.github.com>
2020-06-13 17:09:28 +0200

    added main class

---
+++ Main.java
@@ -0,0 +1,67 @@
+
+
+public class Main {
+	public static class Node
+	{
+		public Node(int d)
+		{
+			this.data = d;
+			next=null;
+		}
+		int data;
+		Node next;
+	}
+	public static class LinkedList
+	{
+		public Node head;
+		
+		void remove_duplicates()
+		{
+			Node ptr1=null,ptr2=null;
+			ptr1=head;
+			while(ptr1!=null && ptr1.next!=null)
+			{
+				ptr2=ptr1;
+				while(ptr2.next != null)
+				{
+					if(ptr2.next.data == ptr1.data)
+					{
+						ptr2.next=ptr2.next.next;
+						System.gc();
+					}
+					else
+					{
+						ptr2=ptr2.next;
+					}
+				}
+				ptr1=ptr1.next;
+			}
+		}
+	}
+	static void printList(Node node) { 
+        while (node != null) { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+    } 
+	public static void main(String[] args)
+	{
+		LinkedList list = new LinkedList(); 
+        list.head = new Node(10); 
+        list.head.next = new Node(12); 
+        list.head.next.next = new Node(11); 
+        list.head.next.next.next = new Node(11); 
+        list.head.next.next.next.next = new Node(12); 
+        list.head.next.next.next.next.next = new Node(11); 
+        list.head.next.next.next.next.next.next = new Node(10); 
+  
+        System.out.println("Linked List before removing duplicates : \n "); 
+        printList(list.head); 
+  
+        list.remove_duplicates(); 
+        System.out.println(""); 
+        System.out.println("Linked List after removing duplicates : \n "); 
+        printList(list.head); 	
+	}
+
+}
